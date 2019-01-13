09d930c2f4e79c973bb3c34ce4ba26f8bb2d6706
KajKas <kajetankasperuk@gmail.com>
2019-01-13 16:09:38 +0100

    validates incoming data

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -2,7 +2,9 @@ import * as express from 'express';
 import Controller from '../interfaces/controller.interface';
 import Post from './posts.interface';
 import postModel from './posts.model';
-import PostNotFoundException from '../exceptions/PostNotFoundException'
+import PostNotFoundException from '../exceptions/PostNotFoundException';
+import validationMiddleware from '../middleware/validation.middleware';
+import CreatePostDto from './post.dto';
 
 class PostsController implements Controller {
     public path = '/posts';
@@ -16,9 +18,9 @@ class PostsController implements Controller {
     private initializeRoutes() {
         this.router.get(this.path, this.getAllPosts);
         this.router.get(`${this.path}/:id`, this.getPostById);
-        this.router.put(`${this.path}/:id`, this.modifyPost);
+        this.router.patch(`${this.path}/:id`, validationMiddleware(CreatePostDto, true), this.modifyPost);
         this.router.delete(`${this.path}/:id`, this.deletePost);
-        this.router.post(this.path, this.createPost);
+        this.router.post(this.path, validationMiddleware(CreatePostDto), this.createPost);
     }
 
     private getAllPosts = (request: express.Request, response: express.Response) => {
