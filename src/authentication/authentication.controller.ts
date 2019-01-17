ea7834d9294addf0d0421417a5dc928cd36ac110
KajKas <kajetankasperuk@gmail.com>
2019-01-17 10:16:18 +0100

    Using the authentication middleware

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -7,6 +7,8 @@ import validationMiddleware from '../middleware/validation.middleware';
 import CreateUserDto from '../users/user.dto';
 import userModel from './../users/user.model';
 import LogInDto from './logIn.dto';
+import TokenData from '../interfaces/tokenData.interface';
+import authMiddleware from '../middleware/auth.middleware'
 
 class AuthenticationController implements Controller {
     public path = '/auth';
@@ -18,8 +20,13 @@ class AuthenticationController implements Controller {
     }
 
     private initializeRoutes() {
-        this.router.post(`${this.path}/register`, validationMiddleware(CreateUserDto), this.registration);
-        this.router.post(`${this.path}/login`, validationMiddleware(LogInDto), this.loggingIn);
+        this.router.get(this.path, this.getAllPosts);
+        this.router.get(`${this.path}/:id`, this.getPostById);
+        this.router
+            .all(`${this.path}/*`, authMiddleware)
+            .patch(`${this.path}/:id`, validationMiddleware(CreatePostDto, true), this.modifyPost)
+            .delete(`${this.path}/:id`, this.deletePost)
+            .post(this.path, authMiddleware, validationMiddleware(CreatePostDto), this.createPost);
     }
 
     private registration = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
@@ -35,9 +42,12 @@ class AuthenticationController implements Controller {
                 password: hashedPassword,
             });
             user.password = undefined;
+            const tokenData = this.createToken(user);
+            response.setHeader('Set-Cookie', [this.createCookie(tokenData)]);
             response.send(user);
         }
     }
+
 
     private loggingIn = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
         const logInData: LogInDto = request.body;
@@ -46,6 +56,8 @@ class AuthenticationController implements Controller {
             const isPasswordMatching = await bcrypt.compare(logInData.password, user.password);
             if (isPasswordMatching) {
                 user.password = undefined;
+                const tokenData = this.createToken(user);
+                response.setHeader('Set-Cookie', [this.createCookie(tokenData)]);
                 response.send(user);
             } else {
                 next(new WrongCredentialsException());
@@ -53,6 +65,10 @@ class AuthenticationController implements Controller {
         } else {
             next(new WrongCredentialsException());
         }
+    }
+
+    private createCookie(tokenData: TokenData) {
+        return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn}`;
     }
 }
 
